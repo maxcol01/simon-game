9c9d766e16c76153e36461434698fe10a780f60a
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-04 17:49:32 +0200

    add the detection of button and corrsponding sound

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Linked")
+// detection of which button is pressed and play the corresponding sound
+$(".btn").on("click",(event)=>{
+    $("h1").html("Level 1")
+    let buttonColor = event.target.id;
+    let audio = new Audio(`/sounds/${buttonColor}.mp3`)
+    audio.play()
+})
+
+
