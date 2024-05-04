6668292b7487dd297f70126cb6915a98b7e298b5
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-04 18:05:48 +0200

    add the starting of the game wiht random selection and sound

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// detection of which button is pressed and play the corresponding sound
-$(".btn").on("click",(event)=>{
-    $("h1").html("Level 1")
+// create the event that will trigger the game to start
+document.addEventListener("keydown",()=>{
+    $("h1").html("Level 1");
+    // choice of random starting point --> choice among 4 an play the corresponding sound.
+    const listColor = ["blue","green","yellow","red"];
+    let randomColor = listColor[Math.floor(Math.random()*4)];
+    $(`.${randomColor}`).addClass("pressed");
+    let audio = new Audio(`/sounds/${randomColor}.mp3`);
+    audio.play();
+    setTimeout(()=>{
+        $(`.${randomColor}`).removeClass("pressed");
+    },50)
+})
+
+// detection of which button is pressed and play the corresponding sound (using jQuery)
+$(".btn").on("click",(event)=>{ 
     let buttonColor = event.target.id;
-    let audio = new Audio(`/sounds/${buttonColor}.mp3`)
-    audio.play()
+    let audio = new Audio(`/sounds/${buttonColor}.mp3`);
+    audio.play();
+    // when pressed, do not forget to indicate it using pressed class.
 })
 
 
