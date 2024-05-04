db6b752222abbdcfc5be11a2835be9c44e7cb85e
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-04 18:43:54 +0200

    add of the first layer of memory i.e., arrays creaction

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // function used to generate a press and change in color of a button
+const listColor = ["blue","green","yellow","red"];
+let listButton = [];
+let listUser = [];
 const randomButton = () =>{
-    const listColor = ["blue","green","yellow","red"];
     let randomColor = listColor[Math.floor(Math.random()*4)];
     $(`.${randomColor}`).addClass("pressed");
     let audio = new Audio(`/sounds/${randomColor}.mp3`);
@@ -8,13 +10,16 @@ const randomButton = () =>{
     setTimeout(()=>{
         $(`.${randomColor}`).removeClass("pressed");
     },50)
+    return randomColor
 }
 
 // create the event that will trigger the game to start and selection of a 
 // random button
 document.addEventListener("keydown",()=>{
     $("h1").html("Level 1");
-    randomButton();
+    let seleltButton = randomButton();
+    listButton.push(seleltButton);
+    console.log(listButton);
 })
 
 // detection of which button is pressed and play the corresponding sound (using jQuery)
@@ -23,6 +28,8 @@ $(".btn").on("click",(event)=>{
     let audio = new Audio(`/sounds/${buttonColor}.mp3`);
     audio.play();
     // when pressed, do not forget to indicate it using pressed class.
+    listUser.push(buttonColor);
+    console.log(listUser);
 })
 
 
