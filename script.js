fc1fc0854dc7e34b4f752be7d7d6f1335e6a0949
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2024-05-06 18:20:00 +0200

    try with a function star

--- script.js
+++ script.js
@@ -3,9 +3,18 @@
 
 // function used to generate a press and change in color of a button
 const listColor = ["blue","green","yellow","red"];
-let listButton = [];
+
 let level = 2;
 let listUser = [];
+
+const startGame = () => {
+    let listButton = [];
+    document.addEventListener("keydown",()=>{
+        $("h1").html("Level 1");
+        listButton.push(randomButton()); 
+    })
+}
+
 const rendering = (color) =>{
     $(`.${color}`).addClass("pressed");
     let audio = new Audio(`./sounds/${color}.mp3`);
@@ -21,33 +30,41 @@ const randomButton = () =>{
     return randomColor
 }
 
-// create the event that will trigger the game to start and selection of a 
-// random button
-document.addEventListener("keydown",()=>{
-    $("h1").html("Level 1");
-    listButton.push(randomButton());   
-})
 
+// startGame()
+startGame();
+
+//checht the user input
+$(".btn").on("click",(event) =>{
+    let buttonColor = event.target.id;
+    rendering(buttonColor);
+    if (buttonColor == listButton[0]){
+        ("h1").html("Level 2");
+    }else{
+        ("h1").html("Game over");
+        startGame();
+    }
+})
 
 
 // detection of which button is pressed and play the corresponding sound (using jQuery)
-$(".btn").on("click",(event)=>{ 
-    let i = 0
-    while(i < listButton.length){
-        let buttonColor = event.target.id;
-        rendering(buttonColor);
-        listUser.push(buttonColor);
-        console.log(buttonColor)
-        console.log(listButton[0])
-        if(buttonColor == listButton[i]){
-            console.log("je passe par là")
-            i++
-        }else{
-            $("h1").html("Game over");
-            break
-        }
-    }
-    $("h1").html(`Level ${level++}`);
-    setTimeout(() =>listButton.push(randomButton()),1000)
-    // once clicked that we need to check !
-})
+// $(".btn").on("click",(event)=>{ 
+//     let i = 0
+//     while(i < listButton.length){
+//         let buttonColor = event.target.id;
+//         rendering(buttonColor);
+//         listUser.push(buttonColor);
+//         console.log(buttonColor)
+//         console.log(listButton[0])
+//         if(buttonColor == listButton[i]){
+//             console.log("je passe par là")
+//             i++
+//         }else{
+//             $("h1").html("Game over");
+//             break
+//         }
+//     }
+//     $("h1").html(`Level ${level++}`);
+//     setTimeout(() =>listButton.push(randomButton()),1000)
+//     // once clicked that we need to check !
+// })
