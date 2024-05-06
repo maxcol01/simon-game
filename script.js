5d86814d449f5c86211f9a24b54c7264e2008fc5
maxcol01 <maxcollet01@gmail.com>
2024-05-06 13:27:23 +0200

    first try to have interaction

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // function used to generate a press and change in color of a button
 const listColor = ["blue","green","yellow","red"];
 let listButton = [];
+let level = 2;
 let listUser = [];
-let level = 1;
-
 const rendering = (color) =>{
     $(`.${color}`).addClass("pressed");
-    let audio = new Audio(`/sounds/${color}.mp3`);
+    let audio = new Audio(`./sounds/${color}.mp3`);
     audio.play();
     setTimeout(()=>{
         $(`.${color}`).removeClass("pressed");
@@ -23,12 +22,29 @@ const randomButton = () =>{
 // random button
 document.addEventListener("keydown",()=>{
     $("h1").html("Level 1");
-    listButton.push(randomButton());
+    listButton.push(randomButton());   
 })
+
+
 
 // detection of which button is pressed and play the corresponding sound (using jQuery)
 $(".btn").on("click",(event)=>{ 
-    let buttonColor = event.target.id;
-    rendering(buttonColor);
-    listUser.push(buttonColor);
+    let i = 0
+    while(i < listButton.length){
+        let buttonColor = event.target.id;
+        rendering(buttonColor);
+        listUser.push(buttonColor);
+        console.log(buttonColor)
+        console.log(listButton[0])
+        if(buttonColor == listButton[i]){
+            console.log("je passe ici")
+            i++
+        }else{
+            $("h1").html("Game over");
+            break
+        }
+    }
+    $("h1").html(`Level ${level++}`);
+    setTimeout(() =>listButton.push(randomButton()),1000)
+    // once clicked that we need to check !
 })
